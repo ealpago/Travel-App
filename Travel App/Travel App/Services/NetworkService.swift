cca8f446de4f9a1710173d606106f1a145f7dfc3
ealpago <alpagoemre@icloud.com>
2022-03-14 14:29:13 +0300

    NetworkService added

--- Travel App/Travel App/Services/NetworkService.swift
+++ Travel App/Travel App/Services/NetworkService.swift
@@ -5,7 +5,7 @@
 //  Created by Emre Alpago on 10.03.2022.
 //
 
-//https://pixabay.com/api/?key=26092409-e7336837b22977dd15edd88a0
+//https://api.unsplash.com/search/photos?query=profile+picture&client_id=64qv8DmNP_ztBDEq-4eQ9r7tVQtj3dvYcijhPQWhusM
 
 import Foundation
 
@@ -18,23 +18,39 @@ public enum HTTPMethod:String{
 }
 
 public enum NetworkRouter{
-    case category(category:String)
+    case query(query:String)
     
     public var method:HTTPMethod {
         switch self{
-        case .category:
+        case .query:
             return .get
         }
     }
-
     
-//    public var path:String{
-//        switch self {
-//        case .category(let category):
-//            return Constants.shared.baseURL
-//        }
-//    }
-    
-    
+    public var path:String{
+        switch self {
+        case .query(let query):
+            return APIConstants.shared.unsplashBaseURL + "search/photos?query=\(query)" + APIConstants.shared.unsplashAPI_KEY
+        }
+    }
 }
 
+class NetworkManager {
+    static let service = NetworkManager()
+    public func request<T: Codable>(requestRoute: NetworkRouter, responseModel:T.Type, completion:@escaping (_ details: T) -> Void){
+        if let url = URL(string: requestRoute.path){
+            var request = URLRequest(url: url)
+            request.httpMethod = requestRoute.method.rawValue
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: request) { data, response, error in
+                do {
+                    let data = try JSONDecoder().decode(T.self, from: data!)
+                    completion(data)
+                } catch let e as NSError {
+                    print("error : \(e)")
+                }
+            }
+            task.resume()
+        }
+    }
+}
